b310eb7596566a3779319c7fd533f430d46908a8
MINSSUNGKIM <163739681+MINSSUNGKIM@users.noreply.github.com>
2024-07-16 08:27:56 +0900

    [Bronze II] Title: Hashing, Time: 172 ms, Memory: 17712 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		long N = sc.nextInt();
+		
+		long ans=0;
+		long r =1;
+		long m = 1234567891;
+		String str = sc.next();
+		
+		for(int i=0; i<N; ++i) {
+		
+			 long p =(int)str.charAt(i)-96;
+			 ans += (p*r)%m;
+			 r = (r*31)%m;
+		}
+		
+		System.out.println(ans);
+		
+	}
+
+}
