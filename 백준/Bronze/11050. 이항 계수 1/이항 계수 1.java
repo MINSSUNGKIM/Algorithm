aa76db8082ffc2bf5ed159bb3206ee66da39b453
MINSSUNGKIM <163739681+MINSSUNGKIM@users.noreply.github.com>
2024-07-16 12:03:14 +0900

    [Bronze I] Title: 이항 계수 1, Time: 172 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		
+		long ans = factorial(n)/(factorial(n-k)*factorial(k));
+		
+		System.out.println(ans);
+		
+
+		
+	}
+	
+	static long factorial(int N) {
+		
+		long ans=1;
+		while (N>1) {
+			ans = ans*N;
+			N--;
+		}
+		
+		return ans;
+	}
+
+}
