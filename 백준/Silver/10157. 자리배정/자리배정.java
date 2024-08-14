7fc8c89ba9b5693fef57fe1b22118ed12a12f2be
MINSSUNGKIM <163739681+MINSSUNGKIM@users.noreply.github.com>
2024-08-14 09:24:50 +0900

    [Silver III] Title: 자리배정, Time: 144 ms, Memory: 15268 KB -BaekjoonHub

--- 백준/Silver/10157. 자리배정/자리배정.java
+++ 백준/Silver/10157. 자리배정/자리배정.java
@@ -4,41 +4,54 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int c = Integer.parseInt(st.nextToken());
-        int r = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(br.readLine());
-        int[][] arr = new int[c][r];
-        int[] dx = {0, 1, 0, -1};
-        int[] dy = {1, 0, -1, 0};
-        int x = 0;
-        int y = 0;
-        int nx, ny;
-        int d = 0;
-        int num = 1;
 
-        if (c * r < k) {
-            System.out.println(0);
-            return;
-        }
+	public static void main(String[] args) throws IOException {
 
-        while (num < k) {
-            arr[x][y] = num++;
-            nx = x + dx[d];
-            ny = y + dy[d];
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(br.readLine());
+		if (K > r * c) {
+			System.out.println(0);
+			br.close();
+			return;
+		}
 
-            if (0 > nx || nx >= c || 0 > ny | ny >= r || arr[nx][ny] != 0) {
-                d = ++d % 4;
-                nx = x + dx[d];
-                ny = y + dy[d];
-            }
+		boolean[][] map = new boolean[r][c];
 
-            x = nx;
-            y = ny;
-        }
+		int[] dr = { 0, 1, 0, -1 };
+		int[] dc = { 1, 0, -1, 0 };
 
-        System.out.println((x+1) + " " + (y+1));
-    }
+		int nr = 0;
+		int nc = 0;
+		int x = 0;
+		int y = 0;
+		int k = 1;
+		int index = 0;
+		while (k < K) {
+			map[nr][nc] = true;
+			nr = x + dr[index];
+			nc = y + dc[index];
+
+			k++;
+
+			if (nr < 0 || nr >= r || nc < 0 || nc >= c || map[nr][nc]) {
+				index = (index + 1) % 4;
+				nr = x + dr[index];
+				nc = y + dc[index];
+			}
+
+			x = nr;
+			y = nc;
+		}
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(nr + 1).append(" ").append(nc + 1);
+		System.out.println(sb.toString());
+
+		br.close();
+
+	}
+
 }
